af749ab714daa1638ec56ee81de5573fdd628455
Chris <89666114+chrislqiu@users.noreply.github.com>
2022-05-27 16:20:56 -0400

    Update Game.java
    added random index to choose random word from txt file

--- Game.java
+++ Game.java
@@ -7,10 +7,13 @@ import java.util.ArrayList;
 public class Game {
 
     private static ArrayList<String> lib = new ArrayList<>();
+    static String[][] board = new String[6][5];
+    private String wordToBeGuessed = "";
 
     public static void readWords() {
         try {
-            Scanner sc = new Scanner(new BufferedReader(new FileReader("src\\Words.txt")));
+            Scanner sc = new Scanner(new BufferedReader(new FileReader
+                    ("C:\\Users\\Chris\\IdeaProjects\\Jordle\\src\\Words")));
             while (sc.hasNext()) {
                 String word = sc.nextLine();
                 lib.add(word);
@@ -18,5 +21,13 @@ public class Game {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        readWords();
+        Scanner sc = new Scanner(System.in);
+        int ranIndex = (int) (Math.random() * (lib.size() + 1) + 0);
+        String wordToBeGuessed = lib.get(ranIndex);
+        
     }
 }
