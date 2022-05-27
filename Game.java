d1b40b6a9901d12a086eef437b388b995cc4bd31
Chris <89666114+chrislqiu@users.noreply.github.com>
2022-05-27 17:21:16 -0400

    updates

--- Game.java
+++ Game.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -8,7 +9,6 @@ public class Game {
 
     private static ArrayList<String> lib = new ArrayList<>();
     static String[][] board = new String[6][5];
-    private String wordToBeGuessed = "";
 
     public static void readWords() {
         try {
@@ -28,6 +28,23 @@ public class Game {
         Scanner sc = new Scanner(System.in);
         int ranIndex = (int) (Math.random() * (lib.size() + 1) + 0);
         String wordToBeGuessed = lib.get(ranIndex);
-        
+        System.out.println("Welcome to Jordle! \n\n If the word contains the letter and is in the wrong position the" +
+                " letter will be followed by a '*'.\n If the letter is not apart of the word, the letter will be " +
+                "followed by a '-'.\n Start guessing! (type in all lowercase, thanks!)");
+        int iniRow = 0;
+        while (iniRow != 6) {
+            String guess = sc.nextLine();
+            for (int i = 0; i < wordToBeGuessed.length(); i++) {
+                if (wordToBeGuessed.charAt(i) == guess.charAt(i)) {
+                    board[iniRow][i] = String.valueOf(guess.charAt(i));
+                } else if (wordToBeGuessed.contains(String.valueOf(guess.charAt(i)))) {
+                    board[iniRow][i] = guess.charAt(i) + "*";
+                } else {
+                    board[iniRow][i] = guess.charAt(i) + "-";
+                }
+            }
+            System.out.println(Arrays.deepToString(board));
+            iniRow++;
+        }
     }
 }
